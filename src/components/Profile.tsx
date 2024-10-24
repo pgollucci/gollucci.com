905b95df72e114f61f99e75d3a8575a4ba8c2ee4
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-24 15:11:58 -0500

    fix: lighthouse header levels (#162)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,8 +6,8 @@ function Profile(): ReactElement {
   return (
     <>
       <h1 className="prose prose-2xl text-center font-extrabold italic">Philip M. Gollucci</h1>
-      <h4 className="prose p-2 text-center underline"><Link href="mailto:philip@gollucci.com">philip@gollucci.com</Link></h4>
-      <h4 className="prose prose-sm p-2 text-center text-base underline"><a href="https://calendly.com/pgollucci">Schedule a chat</a></h4>
+      <h2 className="prose p-2 text-center underline"><Link href="mailto:philip@gollucci.com">philip@gollucci.com</Link></h2>
+      <h3 className="prose prose-sm p-2 text-center text-base underline"><a href="https://calendly.com/pgollucci">Schedule a chat</a></h3>
       <Image className="rounded-full shadow-2xl" src="/me.jpg" alt="Philip M. Gollucci" width={150} height={350} />
     </>
   )
